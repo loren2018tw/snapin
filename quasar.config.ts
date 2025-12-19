6cf9d7b0c1df296ced06c458f52c2f88fb7a1b6f
Loren Li <loren.tw@gmail.com>
2025-12-19 16:38:49 +0800

    0.00.003

--- quasar.config.ts
+++ quasar.config.ts
@@ -196,7 +196,6 @@ export default defineConfig((/* ctx */) => {
       },
       builder: {
         appId: 'boats.loren.snipin',
-        publish: 'never',
         win: {
           target: [
             {
